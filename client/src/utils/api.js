4617512ca840c603bbe5951982dafe3fb03c459b
DivyanshiBadoni <154444073+DivyanshiBadoni@users.noreply.github.com>
2026-06-12 17:57:54 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE = '/api';
+const BASE = 'https://expense-tracker-server-mytx.onrender.com/api';
 
 async function request(path, options = {}) {
   const res = await fetch(`${BASE}${path}`, {
